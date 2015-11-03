1f78cef57bef0c29da159058ad29d790f8a74711
yyzeng <yyzeng@Ctrip.com>
2015-11-03 20:00:50 +0800

    install supervisor

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-app.engine('.html', ejs.__express);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
+/*app.engine('.html', ejs.__express); //html template
+app.set('view engine', 'html');*/
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
